6df46248942466433a5f2ec5d255350df9ddfb47
Mengstu <mfn@modern-miracle.com>
2025-04-04 12:20:04 +0300

    chore: remove legacy code and reference files

--- led-up-fe/ref/contract-tests/data-registry/index.ts
+++
@@ -1,8 +0,0 @@
-// Import all test files to run them together
-import './BasicFunctionality.test';
-import './ProviderManagement.test';
-import './RecordManagement.test';
-import './ProducerManagement.test';
-import '../DataSharing.test';
-import './AdministrativeFunctions.test';
-import './Compensation.test';
